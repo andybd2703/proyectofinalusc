d857ef8a67c90c2999474217ebeab40d311c683e
andybd2703 <yeison.andres2006.yb@gmail.com>
2024-09-06 15:10:11 -0500

    nuevo

---
+++ a.js
@@ -0,0 +1,6 @@
+function Mostrar(){
+    document.getElementById('img').style.display = 'block';
+}
+function Ocultar(){
+    document.getElementById('img').style.display = 'none';
+}
---
+++ prueba.js
@@ -0,0 +1,37 @@
+function Mostrar() {
+  // Muestra el contenido
+  document.getElementById('img').style.display = 'block';
+
+  // Oculta el botón "Mostrar" y muestra el botón "Ocultar"
+  document.getElementById('mostrar').style.display = 'none';
+  document.getElementById('ocultar').style.display = 'block';
+}
+
+function Ocultar() {
+  // Oculta el contenido
+  document.getElementById('img').style.display = 'none';
+
+  // Muestra el botón "Mostrar" y oculta el botón "Ocultar"
+  document.getElementById('mostrar').style.display = 'block';
+  document.getElementById('ocultar').style.display = 'none';
+}
+
+function Mostrar2() {
+  // Muestra el contenido
+  document.getElementById('img2').style.display = 'block';
+
+  // Oculta el botón "Mostrar" y muestra el botón "Ocultar"
+  document.getElementById('mostrar2').style.display = 'none';
+  document.getElementById('ocultar2').style.display = 'block';
+}
+
+function Ocultar2() {
+  // Oculta el contenido
+  document.getElementById('img2').style.display = 'none';
+
+  // Muestra el botón "Mostrar" y oculta el botón "Ocultar"
+  document.getElementById('mostrar2').style.display = 'block';
+  document.getElementById('ocultar2').style.display = 'none';
+}
+
+
